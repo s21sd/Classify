ddf7d0abddeb15150a05e9df05570c69bf65787e
sunny srivastava <sunnysrivastava359@gmail.com>
2023-05-01 11:46:47 +0530

    New PUSh

--- app/src/main/java/com/example/keepnotes/AboutFragment.java
+++ app/src/main/java/com/example/keepnotes/AboutFragment.java
@@ -1,21 +1,15 @@
 package com.example.keepnotes;
 
 import android.annotation.SuppressLint;
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
-import java.util.Objects;
+import androidx.fragment.app.Fragment;
 
 public class AboutFragment extends Fragment {
 
@@ -32,35 +26,21 @@ public class AboutFragment extends Fragment {
         openlinkedin=view.findViewById(R.id.share_insta);
         openmail=view.findViewById(R.id.share_mail);
 
-        opengit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String giturl="https://github.com/s21sd/Classify";
-                Intent intent=new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse(giturl));
-                startActivity(intent);
-            }
-
+        opengit.setOnClickListener(view1 -> {
+            String giturl="https://github.com/s21sd/Classify";
+            Intent intent=new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(giturl));
+            startActivity(intent);
         });
 
-        openlinkedin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String linkdinaurl="https://www.linkedin.com/in/sunny-srivastava-a82996244";
-                Intent intent=new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse(linkdinaurl));
-                startActivity(intent);
-            }
-
+        openlinkedin.setOnClickListener(view12 -> {
+            String linkdinaurl="https://www.linkedin.com/in/sunny-srivastava-a82996244";
+            Intent intent=new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(linkdinaurl));
+            startActivity(intent);
         });
 
-        openmail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Intent.ACTION_SENDTO,Uri.parse("mailto:srivastavasunny359@gmail.com")));
-            }
-
-        });
+        openmail.setOnClickListener(view13 -> startActivity(new Intent(Intent.ACTION_SENDTO,Uri.parse("mailto:srivastavasunny359@gmail.com"))));
 
 
 
